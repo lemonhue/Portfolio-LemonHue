3de81b4187dec7787569270e46fca53e97bb3143
lemonhue <lemonhue54@gmail.com>
2025-10-01 21:03:56 +0800

    first commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,52 @@
+import LogoTitle from '../../assets/images/logo-s.png'
+import './index.scss'
+import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
+
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+  const nameArray = 'Lobodan'.split('');
+  const jobArray = 'Web Developer'.split('');
+
+  useEffect(() => {
+  const timer = setTimeout(() => {
+    setLetterClass('text-animate-hover');
+  }, 4000);
+
+  return () => clearTimeout(timer); // Proper Cleanup
+}, []);
+
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i</span>
+          <br />
+          <span className={`${letterClass} _12`}>I</span>
+          <span className={`${letterClass} _12`}>'</span>
+          <span className={`${letterClass} _12`}>m</span>
+          <img src={LogoTitle} alt="developer" />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={22}
+          />
+        </h1>
+        <h2> Frontend Developer / Javascript Developer / Illustrator</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Home
