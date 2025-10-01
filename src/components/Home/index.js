6c113fc88addc0ad2b3526692cd101afa626e1ba
lemonhue <lemonhue54@gmail.com>
2025-10-01 22:24:37 +0800

    add Logo component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,6 +3,7 @@ import './index.scss'
 import { Link } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
+import Logo from './Logo'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
@@ -45,6 +46,7 @@ const Home = () => {
           CONTACT ME
         </Link>
       </div>
+      <Logo />
     </div>
   )
 }
