f81428b4eee068ba9ddda3bd49edee6c7851e3f4
lemonhue <lemonhue54@gmail.com>
2025-10-02 14:09:45 +0800

    add card

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,10 +1,18 @@
 import './index.scss'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
 
 const Projects = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
-  const headerArray = ' Projects.'.split('')
+  const headerArray = ' Projects'.split('')
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+
+    return () => clearTimeout(timer) // Proper Cleanup
+  }, [])
 
   return (
     <div className="container projects-page">
@@ -16,6 +24,28 @@ const Projects = () => {
             idx={15}
           />
         </h1>
+      </div>
+
+      <div className="project-list">
+        <grid className="cards">
+          <div className="card">
+            <h1>01</h1>
+            <div  className="card-content-header">
+              <h2 >AssetPro</h2>
+            </div>
+            <div className="card-description">
+              <h2>
+                An Asset Management System built with MERN <br />
+              </h2>
+            </div>
+            <div className="card-link">
+              <span>Link: </span>
+              <a href="https://github.com/lemonhue/AssetPro">
+                https://github.com/lemonhue/AssetPro
+              </a>
+            </div>
+          </div>
+        </grid>
       </div>
     </div>
   )
