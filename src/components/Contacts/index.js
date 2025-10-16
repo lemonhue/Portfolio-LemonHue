78803a88427517b9a3e98497d2c8d50f8e6da4c8
lemonhue <lemonhue54@gmail.com>
2025-10-16 21:31:41 +0800

    refactor contacts page layout and styles

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -1,21 +1,55 @@
 import AnimatedLetters from '../AnimatedLetters'
 import { useState } from 'react'
 import './index.scss'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faLinkedin,
+  faGithub,
+  faYoutube,
+  faSkype,
+  faLinkedinIn,
+} from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
 const Contacts = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
   const headerArray = ' Contact me.'.split('')
 
   return (
+
+    
     <div className="container contacts-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            strArray={headerArray}
-            letterClass={letterClass}
-            idx={15}
-          />
-        </h1>
+      
+      <div className="text-zone-main">
+        <ul>
+          <li>
+            <a>
+              <FontAwesomeIcon
+                icon={faGithub}
+                color="#4d4d4e"
+                className="anchor-icon"
+              />
+            </a>
+          </li>
+          <li>
+            <a>
+              <FontAwesomeIcon
+                icon={faEnvelope}
+                color="#4d4d4e"
+                className="anchor-icon"
+              />
+            </a>
+          </li>
+
+          <li>
+            <a>
+              <FontAwesomeIcon
+                icon={faLinkedinIn}
+                color="#4d4d4e"
+                className="anchor-icon"
+              />
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   )
