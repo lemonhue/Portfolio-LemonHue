6694058e368a3d3c23e68e650f898c3abf60fbc2
lemonhue <lemonhue54@gmail.com>
2025-10-18 11:59:50 +0800

    add icons

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,17 @@ import { Link } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
 import Logo from './Logo'
+import Firebase from '../../assets/images/firebase.png'
+import React from '../../assets/images/React.png'
+import Next from '../../assets/images/Next.png'
+import NodeJS from '../../assets/images/NodeJS.png'
+import Github from '../../assets/images/github.png'
+import HTML from '../../assets/images/html.png'
+import CSS from '../../assets/images/css.png'
+import JavaScript from '../../assets/images/javascript.png'
+import Express from '../../assets/images/Express.png'
+import Postman from '../../assets/images/postman.png'
+import MongoDB from '../../assets/images/MongoDB.png'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -42,9 +53,47 @@ const Home = () => {
           />
         </h1>
         <h2> Frontend Developer / Javascript Developer / Illustrator</h2>
+
         <Link to="/contacts" className="flat-button">
           CONTACT ME
         </Link>
+      </div>
+      <div className="skills">
+        <ul>
+          <li>
+            <img src={React} alt="React" />
+          </li>
+          <li>
+            <img src={JavaScript} alt="JavaScript" />
+          </li>
+          <li>
+            <img src={HTML} alt="HTML" />
+          </li>
+          <li>
+            <img src={CSS} alt="CSS" />
+          </li>
+          <li>
+            <img src={Next} alt="Next" />
+          </li>
+          <li>
+            <img src={NodeJS} alt="Node" />
+          </li>
+          <li>
+            <img src={Express} alt="Express" />
+          </li>
+          <li>
+            <img src={MongoDB} alt="MongoDB" />
+          </li>
+          <li>
+            <img src={Firebase} alt="Firebase" />
+          </li>
+          <li>
+            <img src={Postman} alt="Postman" />
+          </li>
+          <li>
+            <img src={Github} alt="Github" />
+          </li>
+        </ul>
       </div>
       <Logo />
     </div>
