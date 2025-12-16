c6294d2c231c2281b671e291f7a44fa609a854e7
lemonhue <lemonhue54@gmail.com>
2025-12-16 21:21:05 +0800

    style: update media queries

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -18,7 +18,7 @@ import MongoDB from '../../assets/images/MongoDB.png'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
-  const nameArray = ' Lemonhue.'.split('')
+  const nameArray = `Hi, I'm Lemonhue.`.split('')
   const jobArray = 'A Web Developer based in PH.'.split('')
 
   useEffect(() => {
@@ -30,73 +30,69 @@ const Home = () => {
   }, [])
 
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i</span>
-          <span className={`${letterClass} _12`}>,</span>
-          <br />
-          <span className={`${letterClass} _12`}>I</span>
-          <span className={`${letterClass} _12`}>'</span>
-          <span className={`${letterClass} _12`}>m</span>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={22}
-          />
-        </h1>
-        <h2> Frontend Developer / Javascript Developer / Illustrator</h2>
+    <div className="container-home-page">
+      <div className="home-page-content">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+
+          <div className="skills">
+            <ul>
+              <li>
+                <img src={React} alt="React" />
+              </li>
+              <li>
+                <img src={JavaScript} alt="JavaScript" />
+              </li>
+              <li>
+                <img src={HTML} alt="HTML" />
+              </li>
+              <li>
+                <img src={CSS} alt="CSS" />
+              </li>
+              <li>
+                <img src={Next} alt="Next" />
+              </li>
+              <li>
+                <img src={NodeJS} alt="Node" />
+              </li>
+              <li>
+                <img src={Express} alt="Express" />
+              </li>
+              <li>
+                <img src={MongoDB} alt="MongoDB" />
+              </li>
+              <li>
+                <img src={Firebase} alt="Firebase" />
+              </li>
+              <li>
+                <img src={Postman} alt="Postman" />
+              </li>
+              <li>
+                <img src={Github} alt="Github" />
+              </li>
+            </ul>
+          </div>
+          <div className="contact-button">
+            <Link to="/contacts" className="flat-button">
+              CONTACT ME
+            </Link>
+          </div>
+        </div>
+
+        <Logo />
       </div>
-      <div className="skills">
-        <ul>
-          <li>
-            <img src={React} alt="React" />
-          </li>
-          <li>
-            <img src={JavaScript} alt="JavaScript" />
-          </li>
-          <li>
-            <img src={HTML} alt="HTML" />
-          </li>
-          <li>
-            <img src={CSS} alt="CSS" />
-          </li>
-          <li>
-            <img src={Next} alt="Next" />
-          </li>
-          <li>
-            <img src={NodeJS} alt="Node" />
-          </li>
-          <li>
-            <img src={Express} alt="Express" />
-          </li>
-          <li>
-            <img src={MongoDB} alt="MongoDB" />
-          </li>
-          <li>
-            <img src={Firebase} alt="Firebase" />
-          </li>
-          <li>
-            <img src={Postman} alt="Postman" />
-          </li>
-          <li>
-            <img src={Github} alt="Github" />
-          </li>
-        </ul>
-      </div>
-      <div className="contact-button">
-        <Link to="/contacts" className="flat-button">
-          CONTACT ME
-        </Link>
-      </div>
-      <Logo />
     </div>
   )
 }
