61cdab558689d7ad210f151d66ae56348b45e5d6
lemonhue <lemonhue54@gmail.com>
2025-12-19 20:14:10 +0800

    style: update

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,28 +8,26 @@ const About = () => {
   const headerArray = ' About    me.'.split('')
 
   return (
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            strArray={headerArray}
-            letterClass={letterClass}
-            idx={15}
-          />
-        </h1>
-
-        <div className="description">
-          <p>
-            I’m Lou, though I go by Lemonhue online. My passion for the arts led me to
-            programming, where I found a perfect balance between creativity and
-            logic. I love creating projects that don’t just work well but also
-            spark inspiration through their visuals.{' '}
-          </p>
-        </div>
-      </div>
-
+    <div className="container-about-page">
       <div className="profile">
         <img src={Me} alt="Background" className="background-image" />
+        <div className="about-page-header">
+          <h1>
+            <AnimatedLetters
+              strArray={headerArray}
+              letterClass={letterClass}
+              idx={15}
+            />
+          </h1>
+        </div>
+        <div className="description">
+          <p>
+            I’m Lou, though I go by Lemonhue online. My passion for the arts led
+            me to programming, where I found a perfect balance between
+            creativity and logic. I love creating projects that don’t just work
+            well but also spark inspiration through their visuals.{' '}
+          </p>
+        </div>
       </div>
     </div>
   )
