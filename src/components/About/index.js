2be157b4c86c7957524077d889b3021685008651
lemonhue <lemonhue54@gmail.com>
2026-01-04 22:49:14 +0800

    feat: edit classnames

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,27 +5,28 @@ import Me from '../../assets/images/me.jpg'
 
 const About = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
-  const headerArray = ' About me.'.split('')
+  const headerArray = ' About '.split('')
 
   return (
     <div className="container-about-page">
       <div className="profile">
         <img src={Me} alt="Background" className="background-image" />
-        <div className="about-page-header">
-          <h1>
-            <AnimatedLetters
-              strArray={headerArray}
-              letterClass={letterClass}
-              idx={15}
-            />
-          </h1>
-        </div>
-        <div className="description">
-          <p>
-            I’m Lou, but I go by Lemonhue online. I love creating projects that don’t just work
-            well but also spark inspiration through their visuals.{' '}
-          </p>
-        </div>
+          <div className="about-page-header">
+            <h1>
+              <AnimatedLetters
+                strArray={headerArray}
+                letterClass={letterClass}
+                idx={15}
+              />
+            </h1>
+          </div>
+          <div className="description">
+            <p>
+              I’m Lou, but I go by Lemonhue online. I love creating projects
+              that don’t just work well but also spark inspiration through their
+              visuals.{' '}
+            </p>
+          </div>
       </div>
     </div>
   )
